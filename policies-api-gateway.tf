137eba776bb77815d8c1fa0fe9ec32d61f10175d
Phil Wilkins <phil@mp3monster.org>
2022-07-11 15:47:45 +0100

    aligned naming to standards

--- policies-api-gateway.tf
+++ policies-api-gateway.tf
@@ -1,12 +1,11 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at oss.oracle.com/licenses/upl
 
-resource "oci_identity_policy" "APIGatewayAccessPolicy" {
+resource "oci_identity_policy" "api_gateway_access_policy" {
   count = contains(var.activate_policies_for_service, "APIGW") ? 1 : 0
 
-  depends_on     = [oci_identity_dynamic_group.FunctionsServiceDynamicGroup]
-  name           = "APIGatewayAccessPolicy-${var.random_id}"
-  description    = "APIGatewayAccessPolicy-${var.random_id}"
+  name           = "api_gateway_access_policy-${var.random_id}"
+  description    = "api_gateway_access_policy-${var.random_id}"
   compartment_id = var.policy_compartment_ocid
   statements = ["Allow group ${var.policy_for_group} to manage api-gateway-family in compartment id ${var.policy_compartment_ocid}",
   "Allow group ${var.policy_for_group} to manage api-gateway-family in compartment id ${var.policy_compartment_ocid}"]
