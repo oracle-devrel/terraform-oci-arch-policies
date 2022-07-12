b30521becf715a332db0715f05d99d03162081f5
Phil Wilkins <phil@mp3monster.org>
2022-07-12 16:24:05 +0100

    missing provider declaration

--- examples/example-create-all-policies/provider.tf
+++ examples/example-create-all-policies/provider.tf
@@ -29,6 +29,17 @@ provider "oci" {
   disable_auto_retries = "true"
 }
 
+
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+
+  #  user_ocid        = var.user_ocid
+  #  fingerprint      = var.fingerprint
+  #  private_key_path = var.private_key_path
+}
+
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
