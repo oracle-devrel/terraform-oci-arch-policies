d174c082136eeea9bf59020eb96a1900b72a84b7
Phil Wilkins <phil@mp3monster.org>
2022-07-12 16:07:11 +0100

    corrected the output var name

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 ## Copyright (c) 2022, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-output "functions_dynamic_group_name" {
+output "functions_dynamic_group" {
   description = "Some policies are dependent on having a dynamic group - if if the module is told to create the dynamic group then the resource is made available here"
   value       = oci_identity_dynamic_group.FunctionsServiceDynamicGroup
 }
