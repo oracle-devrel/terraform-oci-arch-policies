c7b5aa9723c5cdb7b652c41db06dbeb06a4aff44
Phil Wilkins <mp3monster@users.noreply.github.com>
2022-07-14 14:37:16 +0100

    Add files via upload
    align  to tags module

--- examples/example-create-all-policies/policies.tf
+++ examples/example-create-all-policies/policies.tf
@@ -1,17 +1,14 @@
 ## Copyright (c) 2021, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-resource "random_id" "tag" {
-  byte_length = 2
-}
 
 module "example_create_all_policies" {
-  source                        = "github.com/oracle-devrel/terraform-oci-arch-policies"
-  providers                     = { oci = oci.homeregion }
+  #source = "github.com/oracle-devrel/terraform-oci-arch-policies"
+  source = "../../../terraform-oci-arch-policies"
+
+  #providers                     = { oci = oci.homeregion }
   activate_policies_for_service = ["Functions", "OKE", "OpenSearch", "OpenSearchUser"]
   tenancy_ocid                  = var.tenancy_ocid
   policy_compartment_ocid       = var.compartment_ocid
-  random_id                     = "${random_id.tag.hex}"
   region_name                   = "us-ashburn-1"
-  # defined_tags 
 }
