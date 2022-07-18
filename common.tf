0f99aea8371054821b48d2a33edb6ecdf6ea185a
Phil Wilkins <phil@mp3monster.org>
2022-07-18 11:38:36 +0100

    cascade the random_id into the tags module

--- common.tf
+++ common.tf
@@ -2,9 +2,10 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at oss.oracle.com/licenses/upl
 
 module "tags" {
-  #source           = "../terraform-oci-arch-tags"
-  source           = "github.com/oracle-devrel/terraform-oci-arch-tags"
+  source = "../terraform-oci-arch-tags"
+  #source           = "github.com/oracle-devrel/terraform-oci-arch-tags"
   tag_namespace    = "terraform-oci-arch-test-name"
   compartment_ocid = var.compartment_ocid
   release          = var.release
+  random_id        = var.random_id # provide the optional value
 }
