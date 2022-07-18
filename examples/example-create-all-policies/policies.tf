89276efc7216623852229aefe2b94755ab656ca3
Phil Wilkins <phil@mp3monster.org>
2022-07-18 08:53:37 +0100

    adjust parameters to use variables

--- examples/example-create-all-policies/policies.tf
+++ examples/example-create-all-policies/policies.tf
@@ -3,12 +3,13 @@
 
 
 module "example_create_all_policies" {
-  #source = "github.com/oracle-devrel/terraform-oci-arch-policies"
-  source = "../../../terraform-oci-arch-policies"
+  #source = "./../../"
+  source = "github.com/oracle-devrel/terraform-oci-arch-policies"
 
   #providers                     = { oci = oci.homeregion }
-  activate_policies_for_service = ["Functions", "OKE", "OpenSearch", "OpenSearchUser"]
+  activate_policies_for_service = ["Functions", "OKE", "OpenSearch", "OpenSearchUser", "OpenSearch"]
   tenancy_ocid                  = var.tenancy_ocid
-  policy_compartment_ocid       = var.compartment_ocid
-  region_name                   = "us-ashburn-1"
+  compartment_ocid              = var.compartment_ocid
+  region_name                   = var.region
+  release                       = "2.0"
 }
