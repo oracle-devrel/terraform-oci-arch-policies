25574712b21ac31b937b8802921ead5bfbe7ddb6
Phil Wilkins <phil@mp3monster.org>
2022-07-18 08:58:44 +0100

    tidy up modules and sharing of variables

--- common.tf
+++ common.tf
@@ -2,14 +2,9 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at oss.oracle.com/licenses/upl
 
 module "tags" {
-  source = "github.com/oracle-devrel/terraform-oci-arch-tags"
-
+  #source           = "../terraform-oci-arch-tags"
+  source           = "github.com/oracle-devrel/terraform-oci-arch-tags"
   tag_namespace    = "terraform-oci-arch-test-name"
-  tenancy_ocid     = var.tenancy_ocid
-  compartment_ocid = var.policy_compartment_ocid
-  release          = "1.1"
-  region           = ""
-  fingerprint      = ""
-  private_key_path = ""
-  user_ocid        = ""
+  compartment_ocid = var.compartment_ocid
+  release          = var.release
 }
