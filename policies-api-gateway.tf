147441ebd028fcf3ddc78e36b23b94fb9ebf83bf
Phil Wilkins <phil@mp3monster.org>
2022-07-22 13:02:35 +0100

    rationalize policy declaration

--- policies-api-gateway.tf
+++ policies-api-gateway.tf
@@ -7,8 +7,7 @@ resource "oci_identity_policy" "api_gateway_access_policy" {
   name           = "APIGatewayAccessPolicy-${module.tags.random_id}"
   description    = "APIGatewayAccessPolicy-${module.tags.random_id} - group manage  api-gateway-family"
   compartment_id = var.compartment_ocid
-  statements = ["Allow group ${var.policy_for_group} to manage api-gateway-family in compartment id ${var.compartment_ocid}",
-  "Allow group ${var.policy_for_group} to manage api-gateway-family in compartment id ${var.compartment_ocid}"]
-  defined_tags  = module.tags.predefined_tags
-  freeform_tags = local.implementation_module
+  statements     = ["Allow group ${var.policy_for_group} to manage api-gateway-family in compartment id ${var.compartment_ocid}"]
+  defined_tags   = module.tags.predefined_tags
+  freeform_tags  = local.implementation_module
 }
